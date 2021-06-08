cdc51a5e0f523740916edec1bf0f294f4f816169
刘胜华 <3542659850@qq.com>
2021-06-08 15:51:08 +0800

    /

---
+++ personal/动画.js
@@ -0,0 +1,20 @@
+// vue动画的理解
+// 1）操作css的trasition或animation
+// 2)vue会给目标元素添加/移除特定的class
+// 3)过渡的相关类名
+// xxx-enter-active:指定显示的trasition
+// xxx-leave-active:指定隐藏的trasition
+// xxx-enter/xxx-leave-to:指定隐藏时的样式
+
+
+// 常用内置指令
+// v：text:更新元素的textContent
+// v-html:更新元素的inner HTML
+// v-if:如果为true,当前标签才会输出到页面
+// v-else:如果为false,当前标签才会输出到页面
+// v-show：通过控制display样式来控制显示/隐藏
+// v-for:遍历数组/对象
+// v-on:绑定事件监听，一般简写为@
+// v-bind:强制绑定解析表达式，可以省略v-bind
+// v-model:双向数据绑定
+// ref:为某一个元素注册一个唯一标识，vue对象通过$els属性访问这个元素对象
---
+++ personal/生命周期.js
@@ -0,0 +1,27 @@
+生命周期三个阶段：初始化，更新，死亡
+1，初始化显示
+beforeCreate()
+created()
+beforeMount()
+mounted()
+
+2,更新状态
+beforeUpdate()
+updated()
+
+3,销毁vue实例：vm.$destory()
+beforeDestory()
+destoryed()
+
+2，常见的生命周期方法
+mounted():放松ajax请求，启动定时器等异步任务
+
+beforeDestory():做收尾工作，如：清除定时器
+
+mounted:挂载//初始化显示之后立即调用
+
+mounted(){
+    setInterval(()=>{
+        this.show =! this.show
+    })
+}
