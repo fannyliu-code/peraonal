b249a7204e628c93f1dff4e40f6565ec5227064d
sophinaliu <3542659850@qq.com>
2021-06-10 18:03:20 +0800

    /

---
+++ personal/ajax.js
@@ -0,0 +1,35 @@
+// vue项目中常用的2个ajax库
+
+// 1vue-resource
+// vue插件，
+
+// 2，axios
+
+// 3,下载
+// npm install vue-resource --save
+
+// 编码：
+// 引入模块
+// import VueResource from 'vue-resource'
+// 使用插件
+// Vue.use(VueResource)
+
+// 通过vue/组件对象发送ajax请求
+// this.$http.get('/someUrl').then((response)) =>{
+//     //success callback
+//     console.log(response.data)//返回结果数据
+// },(response) =>{
+//     //error callback
+// }
+
+// axios
+// 下载
+// npm install axios --save
+
+// 编码
+// 引入模块
+// import axios from 'axios'
+
+// //发送请求
+
+
