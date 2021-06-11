632cb42fc8c6b666eeba98ec877f81cab6936f0f
sophinaliu <3542659850@qq.com>
2021-06-11 17:06:16 +0800

    /

--- personal/生命周期.js
+++ personal/生命周期.js
@@ -1,27 +1,27 @@
-生命周期三个阶段：初始化，更新，死亡
-1，初始化显示
-beforeCreate()
-created()
-beforeMount()
-mounted()
+// 生命周期三个阶段：初始化，更新，死亡
+// 1，初始化显示
+// beforeCreate()
+// created()
+// beforeMount()
+// mounted()
 
-2,更新状态
-beforeUpdate()
-updated()
+// 2,更新状态
+// beforeUpdate()
+// updated()
 
-3,销毁vue实例：vm.$destory()
-beforeDestory()
-destoryed()
+// 3,销毁vue实例：vm.$destory()
+// beforeDestory()
+// destoryed()
 
-2，常见的生命周期方法
-mounted():放松ajax请求，启动定时器等异步任务
+// 2，常见的生命周期方法
+// mounted():放松ajax请求，启动定时器等异步任务
 
-beforeDestory():做收尾工作，如：清除定时器
+// beforeDestory():做收尾工作，如：清除定时器
 
-mounted:挂载//初始化显示之后立即调用
+// mounted:挂载//初始化显示之后立即调用
 
-mounted(){
-    setInterval(()=>{
-        this.show =! this.show
-    })
-}
+// mounted(){
+//     setInterval(()=>{
+//         this.show =! this.show
+//     })
+// }
